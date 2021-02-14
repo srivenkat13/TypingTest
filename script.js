08708cbff41d2b5c619b0e908b35e63bb3491a63
venkat <srivenkatsairaj13@gmail.com>
2021-02-14 21:49:37 +0530

    Added leading zeros to timer

--- script.js
+++ script.js
@@ -8,11 +8,16 @@ var timer = [0, 0, 0, 0];
 // we want minutes second and milliseconds  along with a variable so we go for array
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
-
+function leadingZero (time){
+    if( time <= 9){
+        time = "0" + time;
+    }
+    return time
+}
 
 // Run a standard minute/second/hundredths timer:
 function runTimer() {
-    let currentTime = timer[0] + ":" + timer[1] + ":" + timer[2]
+    let currentTime = leadingZero(timer[0]) + ":" +leadingZero(timer[1]) + ":" +leadingZero( timer[2]);
     theTimer.innerHTML = currentTime;
     timer[3]++;
 
