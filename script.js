d5afe05d90dd2083ff2e0296f4ed86da1081dcf5
venkat <srivenkatsairaj13@gmail.com>
2021-02-14 21:42:03 +0530

    Added counter functionality to timer

--- script.js
+++ script.js
@@ -4,20 +4,30 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
-var timer = 0 ;
+var timer = [0, 0, 0, 0];
+// we want minutes second and milliseconds  along with a variable so we go for array
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 
 
 // Run a standard minute/second/hundredths timer:
-function runTimer(){
-    theTimer.innerHTML = timer;
-    timer++;
+function runTimer() {
+    let currentTime = timer[0] + ":" + timer[1] + ":" + timer[2]
+    theTimer.innerHTML = currentTime;
+    timer[3]++;
+
+    timer[0] = Math.floor((timer[3]/100/60));//minutes
+    //first the counter/100 gives seconds and seconds/60 gives minutes 
+    timer[1] = Math.floor((timer[3]/100) - (timer[0]*60)); // seconds
+    //first we get pure seconds and minutes are substracted so that everytime we hit a minute this counter goes back to zero
+    timer[2] = Math.floor((timer[3] -(timer[1]*100)- (timer[0]*60*100))); // milliseconds
+    //  first we get pure milliseconds and substract everytime we hit a second also every time a minute is hit
+
 }
 
 
 // Match the text entered with the provided text on the page:
-function spellCheck(){
+function spellCheck() {
     let textEntered = testArea.value;
     console.log(textEntered);
 }
@@ -25,8 +35,8 @@ function spellCheck(){
 // Start the timer:
 function start() {
     let textEnteredLenght = testArea.value.length;
-    if ( textEnteredLenght === 0){
-        setInterval(runTimer,10);
+    if (textEnteredLenght === 0) {
+        setInterval(runTimer, 10);
     }
     console.log(textEnteredLenght);
 }
@@ -36,5 +46,5 @@ function reset() {
 }
 // Event listeners for keyboard input and the reset button:
 testArea.addEventListener("keypress", start, false);
-testArea.addEventListener("keyup",spellCheck, false)
-resetButton.addEventListener("click",reset,false)
+testArea.addEventListener("keyup", spellCheck, false)
+resetButton.addEventListener("click", reset, false)
