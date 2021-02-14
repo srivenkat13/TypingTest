15ec82c5418ce76703150e92830447b9c3e2f3fa
venkat <srivenkatsairaj13@gmail.com>
2021-02-14 21:19:38 +0530

    Setting timer

--- script.js
+++ script.js
@@ -4,11 +4,16 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
+var timer = 0 ;
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 
 
 // Run a standard minute/second/hundredths timer:
+function runTimer(){
+    theTimer.innerHTML = timer;
+    timer++;
+}
 
 
 // Match the text entered with the provided text on the page:
@@ -20,6 +25,9 @@ function spellCheck(){
 // Start the timer:
 function start() {
     let textEnteredLenght = testArea.value.length;
+    if ( textEnteredLenght === 0){
+        setInterval(runTimer,10);
+    }
     console.log(textEnteredLenght);
 }
 // Reset everything:
